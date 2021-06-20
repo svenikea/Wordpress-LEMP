3e32fb10bd4168216f78dcd261a5887e78d28eea
john <nguyenhongky1998@gmail.com>
2021-06-20 19:07:20 +0700

    add -y to yum command

--- startup.sh
+++ startup.sh
@@ -22,13 +22,13 @@ apt () {
 }
 
 yum () {
-	sudo yum update
+	sudo yum update -y
 	sudo yum install -y yum-utils
 	# Add Docker repository
 	sudo yum-config-manager \
 		--add-repo \
 		https://download.docker.com/linux/centos/docker-ce.repo
-	sudo yum update
+	sudo yum update -y 
 	sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose
 }
 
