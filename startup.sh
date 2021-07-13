2929845d991cd41776deddf8748e998e9ad6418f
nguyen.hong.ky <SUN-ASTERISK\nguyen.hong.ky@b122344-pc.sun-asterisk.com>
2021-07-13 14:03:15 +0700

    Fix Ubuntu and Debian package
    - Ubuntu and Debian required separate install on docker
    - Add an if statement to detect when is ubuntu. when is debian
    - Changed the environment variable

--- startup.sh
+++ startup.sh
@@ -13,14 +13,24 @@ apt () {
 		gnupg \
 		lsb-release
 	# Add Docker GPG Key
-	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	# Add Docker repository
-	echo \
-		"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+	if [[ $ID == "debian" ]]
+	then
+		curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo \
+			"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+			$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	elif [[ $ID == "ubuntu" ]]
+	then
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo \
+		"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 		$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt-get update
 	sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
+	fi
 }
+
 
 yum () {
 	sudo yum install -y yum-utils
