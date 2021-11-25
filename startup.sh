1a8ad7510702a8de1a90b978db7ef6b9e28ad058
svenikea <nguyenhongky1998@gmail.com>
2021-11-25 13:57:49 +0700

    fixed typo

--- startup.sh
+++ startup.sh
@@ -173,6 +173,7 @@ sed "s/<key>/$keyname/g" -i ./nginx/my-default.conf
 sed "s/<certificate>/$certname/g" -i ./nginx/my-default.conf
 sed "s/<localhost>/$server_name/g" -i ./nginx/my-default.conf
 sed "s/wordpress/$wp_hostname/g" -i ./nginx/my-default.conf
+sed "s/<php>/$php_container_name/g" -i ./nginx/my-default.conf
 
 # Start Docker Systemd
 sudo systemctl start docker
