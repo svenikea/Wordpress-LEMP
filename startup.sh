6dccf48147c58421df01ede13b6967e7076b29e1
svenikea <nguyenhongky1998@gmail.com>
2022-03-22 21:44:23 +0700

    New Update
    * Removed script input
    * Added Redis Cache
    * Updated template file and .env

--- startup.sh
+++ startup.sh
@@ -106,101 +106,27 @@ fi
 
 clear
 
-# Asking for network name for docker
-read -t 2 -p "Network Name (Default is net): " network_name
-network_name=${network_name:-net}
-
-# Asking for Container name
-## Wordpress 
-read -t 2 -p "Wordpress Container Name (Default is wp): " wp_container_name
-wp_container_name=${wp_container_name:-wp}
-read -t 2 -p "Wordpress  Hostname (Default is wordpress): " wp_hostname
-wp_hostname=${wp_hostname:-wordpress}
-key=$(curl https://api.wordpress.org/secret-key/1.1/salt/)
-
-## Database
-read -t 2 -p "Database Container Name (Default is db): " db_container_name
-db_container_name=${db_container_name:-db}
-read -t 2 -p "Database Hostname (Default is mysql): " db_hostname
-db_hostname=${db_hostname:-mysql}
-read -t 2 -p "Database Database Name (Default is mysql): " db_table
-db_table=${db_table:-mysql}
-read -t 2 -p "Database User Name (Default is user): " db_username
-db_username=${db_username:-user}
-read -t 2 -p "Database User Password (Default is password): " db_password
-db_password=${db_password:-password}
-
-## Website
-read -t 2 -p "Web Container name (Default is web): " web_container_name
-web_container_name=${web_container_name:-web}
-read -t 2 -p "Web hostname (Default is web): " web_hostname
-web_hostname=${web_hostname:-web}
-echo "SSL Connection keys and Cert"
-read -t 2 -p "The key name (Default is server): " keyname
-keyname=${keyname:-server}
-keyname+='.key'
-read -t 2 -p "The Cert name (Default is server): " certname
-certname=${certname:-server}
-certname+='.crt'
-read -t 2 -p "How many days does this key will expires (Default is 365): " days
-days=${days:-365}
-read -t 2 -p "Specify the server name: " 	server_name
-server_name=${server_name:-wordpress-docker.com}
-
-## PHP
-read -t 2 -p "PHP container name (Default is php-alpine): " php_container_name
-php_container_name=${php_container_name:-php-alpine}
-read -t 2 -p "Upload Max File size (Default is 100MB): " file_size
-file_size=${file_size:-100M}
-read -t 2 -p "PHP hostname name (Default is php-alpine): " php_hostname
-php_hostname=${php_hostname:-php-host}
-
 # Creating key for SSL connection
 sudo mkdir -p nginx/ssl
-sudo openssl req -x509 -nodes -newkey rsa:4096 -days ${days} -keyout ./nginx/ssl/${keyname} -out ./nginx/ssl/${certname} -subj "/C=US/ST=GA/L=Atlanta/O=NHK Inc/OU=DevOps Department/CN=wordpress-test.com"
-
-# Export all variable to the environment file
-sed "s/net/$network_name/g" -i ./.env
-sed "s/wp/$wp_container_name/g" -i ./.env
-sed "s/wordpress/$wp_hostname/g" -i ./.env
-sed "s/mysql/$db_hostname/g" -i ./.env
-sed "s/db/$db_container_name/g" -i ./.env
-sed "s/user/$db_username/g" -i ./.env
-sed "s/password/$db_password/g" -i ./.env
-sed "s/mysql/$db_table/g" -i ./.env
-sed "s/nginx/$web_container_name/g" -i ./.env
-sed "s/web/$web_hostname/g" -i ./.env
-sed "s/php-alpine/$php_container_name/g" -i ./.env
-sed "s/php/$php_hostname/g" -i ./.env
-sed "s/post_max_size = <MB>/post_max_size = $file_size/g" -i ./php/php.ini
-sed "s/upload_max_filesize = <MB>/upload_max_filesize = $file_size/g" -i ./php/php.ini
-sed "s/<key>/$keyname/g" -i ./.env
-sed "s/<certificate>/$certname/g" -i ./.env
-sed "s/<key>/$keyname/g" -i ./nginx/my-default.conf
-sed "s/<certificate>/$certname/g" -i ./nginx/my-default.conf
-sed "s/<localhost>/$server_name/g" -i ./nginx/my-default.conf
-sed "s/<localhost>/$server_name/g" -i ./.env
+sudo openssl req -x509 -nodes -newkey rsa:4096 -days 365 -keyout ./nginx/ssl/web.key -out ./nginx/ssl/web.crt -subj "/C=US/ST=GA/L=Atlanta/O=NHK Inc/OU=DevOps Department/CN=wordpress-test.com"
 
 # Start Docker Systemd
 sudo systemctl start docker
 # Run the Docker Compose
 sudo docker network create -d bridge ${network_name}
 sudo docker-compose up -d
-web_ip=$(sudo docker inspect $web_container_name | grep '"IPAddress": "1' | tr -d  '", ' | cut -d ':' -f2)
-database_ip=$(sudo docker inspect $db_container_name | grep '"IPAddress": "1' | tr -d  '", ' | cut -d ':' -f2)
-wordpress_ip=$(sudo docker inspect $wp_container_name | grep '"IPAddress": "1' | tr -d  '", ' | cut -d ':' -f2)
-echo "============================================="
-echo "|     Container      |      IP Address      |"
-echo "---------------------------------------------"
-echo "|		$wp_container_name	   |     $wordpress_ip    		  |"
-echo "---------------------------------------------"
-echo "|  $db_container_name		   |    $database_ip           	  |"
-echo "---------------------------------------------"
-echo "|	$web_container_name		   |	$web_ip 		  |"
-echo "============================================="
-if [[ -z "$wordpress_ip" || -z "$database_ip" || -z "$web_ip" ]];
-then
+web_ip=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(sudo docker-compose ps -q web))
+database_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(sudo docker-compose ps -q db))
+wordpress_ip=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sudo docker-compose ps -q wordpress)
+redis_ip=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sudo docker-compose ps -q cache)
+redis_ping=$(sudo docker exec -it $(sudo docker-compose ps -q wordpress) /bin/sh -c "apk add;redis-cli -h ${redis_ip}  -p 6379 ping")
+status_code=$(curl -s -o /dev/null -w "%{http_code}" localhost)
+if [[ -z "$wordpress_ip" || -z "$database_ip" || -z "$web_ip" || -z "$redis_ip" ]];then
 	exit 1
 else
-	echo "Go to this IP $web_ip or address of this system and go to your browser and check it out"
+	if [[ "$redis_ping" != "PONG" ]]; then
+		exit 1
+	elif [[ "${status_code}" != 200 ]]; then
+		exit 1
+	fi
 fi
